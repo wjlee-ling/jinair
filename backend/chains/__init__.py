fde918ac24aa12853c5fc1d38b9cbe8e00e90b12
wjlee <wjlee.ling@gmail.com>
2024-07-04 14:03:31 +0900

    fix:validator reuse & wrong defaults

---
+++ backend/chains/__init__.py
@@ -0,0 +1,1 @@
+from .flights import get_flights_chain
--- backend/chains/flights.py
+++ backend/chains/flights.py
@@ -5,25 +5,25 @@ from typing import Optional
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field, validator
-from langchain_openai import ChatOpenAI
 
 
 class Flight(BaseModel):
     origin: str = Field(description="origin city or airport of the flight")
     destination: str = Field(description="destination city or aiport of the flight")
     date: str = Field(description="date of the flight")
-    persons: int = Field(description="number of persons for booking")
-    price: Optional[int] = Field(1, description="price of the flight")
+    persons: int = Field(1, description="number of persons for booking")
+    follow_up: str = Field(description="follow up question for necessary entities")
+    price: Optional[int] = Field(None, description="price of the flight")
 
-    @validator("origin")
-    def question_ends_with_question_mark(cls, field):
+    @validator("origin", allow_reuse=True)
+    def postprocess_origin(cls, field):
         if field.endswith("공항"):
             return re.sub("공항", "", field).strip()
 
         return field.strip()
 
-    @validator("destination")
-    def question_ends_with_question_mark(cls, field):
+    @validator("destination", allow_reuse=True)
+    def postprocess_destination(cls, field):
         if field.endswith("공항"):
             return re.sub("공항", "", field).strip()
 
@@ -32,8 +32,8 @@ class Flight(BaseModel):
 
 _template = """Given the user [query], [entities] and [output format] below, you are to:
 1. extract entities from the query \
-2. fill in and update the [entities] with newly extracted entities \
-3. and ask follow-up questions to fill in empty slots for **necessary** entities.
+2. fill in and update the [entities] with newly extracted entities and \
+3. as an airline chatbot ask **follow_up** questions to fill in empty slots for **necessary** entities.
 Make sure you do not make up information.
 
 ## [output format]
@@ -49,8 +49,7 @@ Make sure you do not make up information.
 """
 
 
-def get_flights_chain(model_name: str = "gpt-3.5-turbo-0125"):
-    llm = ChatOpenAI(model_name=model_name, temperature=0.0, verbose=True)
+def get_flights_chain(llm):
     parser = PydanticOutputParser(pydantic_object=Flight)
     prompt = PromptTemplate(
         template=_template,
