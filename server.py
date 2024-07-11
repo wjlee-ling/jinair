afe4a2175775cd7d75bedb9387b97f68de89faa4
wjlee <wjlee.ling@gmail.com>
2024-07-11 11:44:55 +0900

    feat: set CORS

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from langserve import add_routes
+
 
 app = FastAPI(
     title="Jaid API Server",
     version="1.0",
     description="API server for Jaid",
+)
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # add_routes(
