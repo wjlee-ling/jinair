c483d4f2d6453a4a398786c79296b6369857f40a
wjlee <wjlee.ling@gmail.com>
2024-07-11 15:36:33 +0900

    feat: added flight search agent to api

--- server.py
+++ server.py
@@ -1,8 +1,29 @@
+# from backend.chains import (
+#     get_QnA_chain,
+#     get_flight_search_agent,
+# )
+from api.flight_search import run_flight_search
+import os
+
+from typing import Any, List, Union
+from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
 from langserve import add_routes
+from langserve.pydantic_v1 import BaseModel, Field
 
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = "jinair-api"
+
+load_dotenv(find_dotenv())
+
+
+# openai_4o = ChatOpenAI(model_name="gpt-4o", temperature=0, verbose=True)
+# openai = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0, verbose=True)
 
 app = FastAPI(
     title="Jaid API Server",
@@ -20,17 +41,29 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
-# add_routes(
-#     app,
-#     ChatOpenAI(model="gpt-3.5-turbo-0125"),
-#     path="/openai",
-# )
 
-# add_routes(
-#     app,
-#     ChatAnthropic(model="claude-3-haiku-20240307"),
-#     path="/anthropic",
-# )
+class Input(BaseModel):
+    raw_input: str
+    chat_history: List[Union[HumanMessage, AIMessage, FunctionMessage]] = Field(
+        ...,
+        extra={
+            "widget": {"type": "chat", "raw_input": "raw_input", "output": "output"}
+        },
+    )
+
+
+class Output(BaseModel):
+    output: str
+
+
+add_routes(
+    app,
+    RunnableLambda(run_flight_search)
+    .with_types(input_type=Input, output_type=Output)
+    .with_config({"run_name": "run_flight_search"}),
+    path="/flight_search",
+)
+
 
 if __name__ == "__main__":
     import uvicorn
