42071e5449d20aac1512de259c9472d012801954
wjlee <wjlee.ling@gmail.com>
2024-07-12 14:21:38 +0900

    update: added more cautions

--- backend/chains/rag.py
+++ backend/chains/rag.py
@@ -12,6 +12,12 @@ template = """Answer the question based only on the following context. \
 If you need more information to better answer the question, please kindly ask for it. \
 Make sure to answer in the lanuage of the Question. 
 Make sure to include useful and relevant information in the context as much as possible.
+Make sure to answer the question in a complete and concise manner.
+Make sure to answer as if you are an expert in the field and you knew the answer. DO NOT say something "according to the source" and alike.
+항상 예의 있지만 친근하게  답변하기. “해요”, “할게요”, “하실까요” 등 해요체를 사용하고 반말이나 “습니다”는 사용하지 않기
+욕설, 비하 표현, 비속어나 부정적인 표현은 사용하지 않기
+핵심 정보는 ** 볼드 **로 표기하기
+
 
 ## context
 {context}
@@ -32,7 +38,7 @@ def _strip(string):
 
 def get_QnA_chain(llm, retriever=None):
     prompt = ChatPromptTemplate.from_template(template)
-    retriever = retriever or get_trained_kiwi_retriever(CORPUS_PATH, ["Q", "A"])
+    # retriever = retriever or get_trained_kiwi_retriever(CORPUS_PATH, ["Q", "A"])
     rag_chain = (
         {
             "context": itemgetter("input") | retriever | _format_docs,
