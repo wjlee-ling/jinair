c3844e36639fbfbe5a2869ebed9dddea44e5398e
wjlee <wjlee.ling@gmail.com>
2024-07-16 15:48:20 +0900

    fix: api

--- api/QnA.py
+++ api/QnA.py
@@ -18,15 +18,15 @@ class Input(BaseModel):
     input: str
 
 
-class Output(BaseModel):
-    output: str
+# class Output(BaseModel):
+#     output: str
 
 
 QnA_chain = (
     get_QnA_chain(
         llm=ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0, verbose=True),
         retriever=get_pinecone_kiwi_retriever("sparse_encoder.pkl"),
-    )
-    .with_types(input_type=Input, output_type=Output)
+    ).with_types(input_type=Input)
+    # .with_types(input_type=Input, output_type=Output)
     .with_config({"run_name": "run_QnA"})
 )
